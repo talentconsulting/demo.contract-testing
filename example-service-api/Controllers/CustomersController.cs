42a1776af2b4a7f8b3cb1a1938679ebd466a314e
Alasdair Reid <ali.reid@live.co.uk>
2023-02-06 15:36:15 +0000

    Fixes

--- example-service-api/Controllers/CustomersController.cs
+++ example-service-api/Controllers/CustomersController.cs
@@ -24,7 +24,6 @@ public class CustomersController : ControllerBase
     }
 
     [HttpGet(Name = "GetCustomer")]
-    [Route("{id}")]
     public async Task<IActionResult> GetCustomer(string id)
     {
         var customer = await _customerService.GetCustomer(id);
--- example-service-api/Program.cs
+++ example-service-api/Program.cs
@@ -30,5 +30,7 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+app.UseDeveloperExceptionPage();
+
 app.Run();
 
